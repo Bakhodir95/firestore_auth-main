2bc6277dec55c440465372da66a4c012f31b1b75
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-07-06 12:23:35 +0500

    save

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -1,33 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firestore_auth/controllers/chat_controller.dart';
 import 'package:firestore_auth/models/message.dart';
+import 'package:firestore_auth/services/message_firebase_service.dart';
 import 'package:firestore_auth/widgets/sorted_id.dart';
-import 'package:flutter/material.dart';
 
 class ChatsScreen extends StatefulWidget {
   final String email;
-  const ChatsScreen({super.key, required this.email});
+
+  const ChatsScreen({Key? key, required this.email}) : super(key: key);
 
   @override
   State<ChatsScreen> createState() => _ChatsScreenState();
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
-  final typingController = TextEditingController();
+  final TextEditingController typingController = TextEditingController();
   final ChatController _chatViewModel = ChatController();
-  late final String _chatRoomId;
-  final String _currentUserEmail = FirebaseAuth.instance.currentUser!.email!;
-
-  @override
-  void dispose() {
-    typingController.dispose();
-    super.dispose();
-  }
+  late String _chatRoomId;
+  late String _currentUserEmail;
+  final _messageFirebaseService = MessagesFirebaseService();
+  final TextEditingController _textController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    _currentUserEmail = FirebaseAuth.instance.currentUser!.email!;
     _chatRoomId = SortedFunction.generateChatRoomId(
         user1Email: widget.email, user2Email: _currentUserEmail);
   }
@@ -56,41 +55,114 @@ class _ChatsScreenState extends State<ChatsScreen> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Expanded(
-                child: StreamBuilder<QuerySnapshot>(
-              stream: _chatViewModel.getMessages(_chatRoomId),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                var messages = snapshot.data!.docs;
-                return ListView.builder(
-                  reverse: true,
-                  itemCount: messages.length,
-                  itemBuilder: (context, index) {
-                    Message message =
-                        Message.fromQuerySnapshot(messages[index]);
-
-                    return Row(
-                      mainAxisAlignment: _currentUserEmail == message.senderId
-                          ? MainAxisAlignment.end
-                          : MainAxisAlignment.start,
-                      children: [
-                        Container(
-                            padding: EdgeInsetsDirectional.all(10),
-                            decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius: BorderRadius.circular(10),
-                                shape: BoxShape.rectangle),
-                            child: Text(
-                              message.message,
-                              style: TextStyle(fontSize: 25),
-                            )),
-                      ],
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _chatViewModel.getMessages(_chatRoomId),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
                     );
-                  },
-                );
-              },
-            )),
+                  }
+                  if (!snapshot.hasData) {
+                    return Center(child: Text("No messages"));
+                  }
+                  var messages = snapshot.data!.docs;
+                  return ListView.builder(
+                    reverse: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      Message message =
+                          Message.fromQuerySnapshot(messages[index]);
+
+                      return Row(
+                        mainAxisAlignment: _currentUserEmail == message.senderId
+                            ? MainAxisAlignment.end
+                            : MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: _currentUserEmail == message.senderId
+                                  ? Colors.grey
+                                  : Colors.blue,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: GestureDetector(
+                              onDoubleTap: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (ctx) => AlertDialog(
+                                    title: const Text("Delete message"),
+                                    content: Text(
+                                        "Are you sure you want to delete this message?"),
+                                    actions: [
+                                      OutlinedButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text("Cancel"),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          _messageFirebaseService
+                                              .deleteMessages(
+                                                  message.id, _chatRoomId);
+                                          Navigator.pop(context);
+                                          setState(() {});
+                                        },
+                                        child: const Text("Delete"),
+                                      )
+                                    ],
+                                  ),
+                                );
+                              },
+                              onTap: () {
+                                _textController.text = message.message;
+                                showDialog(
+                                  context: context,
+                                  builder: (ctx) => AlertDialog(
+                                    title: const Text("Edit message"),
+                                    content: TextField(
+                                      controller: _textController,
+                                      decoration: const InputDecoration(
+                                        border: OutlineInputBorder(),
+                                      ),
+                                    ),
+                                    actions: [
+                                      OutlinedButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text("Cancel"),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          _messageFirebaseService.editMessages(
+                                              message.id,
+                                              _textController.text,
+                                              _chatRoomId);
+                                          Navigator.pop(context);
+                                          setState(() {});
+                                        },
+                                        child: const Text("Save"),
+                                      )
+                                    ],
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                message.message,
+                                style: const TextStyle(fontSize: 25),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
             TextField(
               controller: typingController,
               decoration: InputDecoration(
--- lib/services/message_firebase_service.dart
+++ lib/services/message_firebase_service.dart
@@ -18,4 +18,29 @@ class MessagesFirebaseService {
   }) {
     _messagesCollection.doc(chatRoomId).collection(_collectedName).add(data);
   }
+
+  Future<void> editMessages(
+      String id, String newMessage, String chatroomId) async {
+    try {
+      await _messagesCollection
+          .doc(chatroomId)
+          .collection('messages')
+          .doc(id)
+          .update({'message': newMessage});
+    } catch (e) {
+      print('Error editing message: $e');
+    }
+  }
+
+  Future<void> deleteMessages(String id, String chatroomId) async {
+    try {
+      await _messagesCollection
+          .doc(chatroomId)
+          .collection("message")
+          .doc(id)
+          .delete();
+    } catch (e) {
+      print('Error deleting message: $e');
+    }
+  }
 }
